f8ff2b2f7dbce5a9045062b2f230a0947e6c92d3
Maggi Yasmin <yasmin.maggi@stud.fhgr.ch>
2026-06-02 16:48:07 +0200

    Dropdown versuch
    Dropdown versuch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 console.log("blub");
+
+console.log("script läuft");
+
+
+
+
+
+
+
+// Elemente holen
+const toggleButton = document.getElementById("toggleButton");
+const dropdown = document.getElementById("dropdown");
+const arrow = document.getElementById("arrow");
+
+// Dropdown öffnen/schließen
+toggleButton.addEventListener("click", () => {
+
+    dropdown.classList.toggle("open");
+
+    arrow.classList.toggle("down");
+    arrow.classList.toggle("up");
+});
+
+
+// Weiße Punkte klickbar machen
+const radios = document.querySelectorAll(".radio");
+
+radios.forEach(radio => {
+
+    radio.addEventListener("click", (e) => {
+
+        e.stopPropagation();
+
+        radio.classList.toggle("active");
+    });
+});
